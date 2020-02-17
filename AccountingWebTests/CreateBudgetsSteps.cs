581994b62196bd5c30dcd65fca886185c91fecc2
Joey Chen <jay.hatelove@gmail.com>
2020-02-17 15:06:02 +0800

    [test] complete create a budget steps definition, and create CreateBudgetPage, CreateBudgetResultPage

--- AccountingWebTests/CreateBudgetsSteps.cs
+++ AccountingWebTests/CreateBudgetsSteps.cs
@@ -1,33 +1,74 @@
 ﻿using System;
+using System.Linq;
+using AccountingWebTests.DataModels;
+using AccountingWebTests.PageObjects;
+using Atata;
 using TechTalk.SpecFlow;
+using TechTalk.SpecFlow.Assist;
 
 namespace AccountingWebTests
 {
     [Binding]
     public class CreateBudgetsSteps : Steps
     {
+        private static CreateBudgetsPage _createBudgetsPage;
+        private CreateBudgetResultPage _createBudgetResultPage;
+
+        [BeforeTestRun]
+        public static void SetUpTestRun()
+        {
+            AtataContext.
+                GlobalConfiguration.
+                UseChrome().
+                //WithArguments("start-maximized").
+                WithLocalDriverPath().
+                UseBaseUrl("http://localhost:50564/").
+                UseCulture("en-us").
+                UseAllNUnitFeatures();
+        }
+
+        [BeforeScenario]
+        public static void SetUpScenario()
+        {
+            AtataContext.Configure().Build();
+
+            _createBudgetsPage = Go.To<CreateBudgetsPage>();
+        }
+
+        [AfterScenario]
+        public static void TearDownScenario()
+        {
+            AtataContext.Current?.CleanUp();
+        }
+
         [Given(@"budget for setting")]
         public void GivenBudgetForSetting(Table table)
         {
-            ScenarioContext.Pending();
+            var budget = table.CreateInstance<Budget>();
+            ScenarioContext.Set(budget);
         }
 
         [When(@"I create")]
         public void WhenICreate()
         {
-            ScenarioContext.Pending();
+            var budget = ScenarioContext.Get<Budget>();
+            _createBudgetResultPage = _createBudgetsPage.Create(budget);
         }
 
-        [Then(@"it should be created successfully")]
+        [Then(@"it should be created succeed")]
         public void ThenItShouldBeCreatedSuccessfully()
         {
-            ScenarioContext.Pending();
+            _createBudgetResultPage.Status.Should.ContainAll("created", "succeed");
         }
 
         [Then(@"there should be budgets existed")]
         public void ThenThereShouldBeBudgetsExisted(Table table)
         {
-            ScenarioContext.Pending();
+            using (var dbContext = new AccountingEntitiesForTest())
+            {
+                var budgets = dbContext.Budgets.ToList();
+                table.CompareToSet(budgets);
+            }
         }
     }
 }
---
+++ AccountingWebTests/PageObjects/CreateBudgetsPage.cs
@@ -0,0 +1,17 @@
+﻿using AccountingWebTests.DataModels;
+using Atata;
+
+namespace AccountingWebTests.PageObjects
+{
+    using _ = CreateBudgetsPage;
+
+    [Url("budgets/create")]
+    public class CreateBudgetsPage : Page<_>
+    {
+        public CreateBudgetResultPage Create(Budget budget)
+        {
+            throw new System.NotImplementedException();
+        }
+
+    }
+}
