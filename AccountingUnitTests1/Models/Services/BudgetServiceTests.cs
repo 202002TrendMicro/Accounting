d9c19276643213d34ffe6cda8391c7b731f6e04c
Joey Chen <jay.hatelove@gmail.com>
2020-02-17 19:51:12 +0800

    [test] add BudgetService Unit Test: "query budget when no budgers"

---
+++ AccountingUnitTests1/Models/Services/BudgetServiceTests.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using AccountingWeb.Models.Services;
+using FluentAssertions;
+using NUnit.Framework;
+
+namespace AccountingUnitTests1.Models.Services
+{
+    [TestFixture()]
+    public class BudgetServiceTests
+    {
+        private BudgetService _budgetService;
+
+        [SetUp]
+        public void SetUp()
+        {
+            _budgetService = new BudgetService();
+        }
+
+        [Test()]
+        public void query_budget_when_no_budgets()
+        {
+            TotalAmountShouldBe(0m,
+                new DateTime(2020, 3, 1),
+                new DateTime(2020, 3, 1));
+        }
+
+        private void TotalAmountShouldBe(decimal expected, DateTime start, DateTime end)
+        {
+            var totalAmount = _budgetService.TotalAmount(start, end);
+            totalAmount.Should().Be(expected);
+        }
+    }
+}
