3f11e5d151cdc2205b7aec3afa14b691f5818ecc
Joey Chen <jay.hatelove@gmail.com>
2020-02-17 14:56:40 +0800

    [test] add a scenario: "create a budget" of feature

---
+++ AccountingWebTests/CreateBudgetsSteps.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using TechTalk.SpecFlow;
+
+namespace AccountingWebTests
+{
+    [Binding]
+    public class CreateBudgetsSteps : Steps
+    {
+        [Given(@"budget for setting")]
+        public void GivenBudgetForSetting(Table table)
+        {
+            ScenarioContext.Pending();
+        }
+
+        [When(@"I create")]
+        public void WhenICreate()
+        {
+            ScenarioContext.Pending();
+        }
+
+        [Then(@"it should be created successfully")]
+        public void ThenItShouldBeCreatedSuccessfully()
+        {
+            ScenarioContext.Pending();
+        }
+
+        [Then(@"there should be budgets existed")]
+        public void ThenThereShouldBeBudgetsExisted(Table table)
+        {
+            ScenarioContext.Pending();
+        }
+    }
+}
