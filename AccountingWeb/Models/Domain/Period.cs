628a37ff89dd14a87cce40091994204dcd05cfc8
Joey Chen <jay.hatelove@gmail.com>
2020-02-17 20:52:13 +0800

    [refactor] move OverlappingDays() to Period class

--- AccountingWeb/Models/Domain/Period.cs
+++ AccountingWeb/Models/Domain/Period.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using AccountingWeb.Models.Entities;
 
 namespace AccountingWeb.Models.Domain
 {
@@ -12,5 +13,17 @@ namespace AccountingWeb.Models.Domain
 
         public DateTime Start { get; private set; }
         public DateTime End { get; private set; }
+
+        public decimal OverlappingDays(Budget budget)
+        {
+            if (Start > budget.LastDay || End < budget.FirstDay)
+            {
+                return 0;
+            }
+
+            var overlappingStart = Start > budget.FirstDay ? Start : budget.FirstDay;
+            var overlappingEnd = End < budget.LastDay ? End : budget.LastDay;
+            return (overlappingEnd - overlappingStart).Days + 1;
+        }
     }
 }
--- AccountingWeb/Models/Services/BudgetService.cs
+++ AccountingWeb/Models/Services/BudgetService.cs
@@ -40,19 +40,7 @@ namespace AccountingWeb.Models.Services
             if (!budgets.Any()) return 0;
 
             var budget = budgets.First();
-            return OverlappingDays(new Period(start, end), budget);
-        }
-
-        private static decimal OverlappingDays(Period period, Budget budget)
-        {
-            if (period.Start > budget.LastDay || period.End < budget.FirstDay)
-            {
-                return 0;
-            }
-
-            var overlappingStart = period.Start > budget.FirstDay ? period.Start : budget.FirstDay;
-            var overlappingEnd = period.End < budget.LastDay ? period.End : budget.LastDay;
-            return (overlappingEnd - overlappingStart).Days + 1;
+            return new Period(start, end).OverlappingDays(budget);
         }
     }
 }
