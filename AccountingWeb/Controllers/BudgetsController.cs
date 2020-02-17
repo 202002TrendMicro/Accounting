4befe07429419567d3bb8ff47a6de095ce043c22
Joey Chen <jay.hatelove@gmail.com>
2020-02-17 15:41:32 +0800

    [feat] add BudgetService class implement IBudgetService

--- AccountingWeb/Controllers/BudgetsController.cs
+++ AccountingWeb/Controllers/BudgetsController.cs
@@ -1,5 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
@@ -10,6 +9,11 @@ namespace AccountingWeb.Controllers
     public class BudgetsController : Controller
     {
         private readonly IBudgetService _budgetService;
+
+        public BudgetsController()
+        {
+            _budgetService = new BudgetService();
+        }
 
         public BudgetsController(IBudgetService budgetService)
         {
---
+++ AccountingWeb/Models/Services/BudgetService.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace AccountingWeb.Models.Services
+{
+    public class BudgetService : IBudgetService
+    {
+        public void Save(string yearMonth, decimal amount)
+        {
+            Console.WriteLine("todo");
+        }
+    }
+}
