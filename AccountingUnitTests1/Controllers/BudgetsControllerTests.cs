4e44bdfa9d4a4a1156d6414efbb426dabb7d891c
Joey Chen <jay.hatelove@gmail.com>
2020-02-17 15:23:53 +0800

    [test] add a unit test project for AccountingWeb application, and add nuget package reference

---
+++ AccountingUnitTests1/Controllers/BudgetsControllerTests.cs
@@ -0,0 +1,14 @@
+﻿using NUnit.Framework;
+
+namespace AccountingUnitTests1.Controllers
+{
+    [TestFixture()]
+    public class BudgetsControllerTests
+    {
+        [Test()]
+        public void CreateBudgetsTest()
+        {
+            Assert.Fail();
+        }
+    }
+}
