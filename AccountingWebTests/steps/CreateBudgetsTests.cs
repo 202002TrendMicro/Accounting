54b6f0b9dadd88858d6806687d2c846f05d7dfe5
Joey Chen <jay.hatelove@gmail.com>
2020-02-19 17:18:05 +0800

    [test] add a web testing via NUnit

---
+++ AccountingWebTests/steps/CreateBudgetsTests.cs
@@ -0,0 +1,46 @@
+﻿using AccountingWebTests.DataModels;
+using AccountingWebTests.PageObjects;
+using Atata;
+using NUnit.Framework;
+
+namespace AccountingWebTests.steps
+{
+    [TestFixture]
+    public class CreateBudgetsTests
+    {
+        private CreateBudgetPage _createBudgetPage;
+
+        [SetUp]
+        public void Setup()
+        {
+            AtataContext.Configure()
+                        .UseChrome()
+                        .UseBaseUrl("http://localhost:50564/")
+                        .UseCulture("en-us").UseNUnitTestName()
+                        .AddNUnitTestContextLogging().LogNUnitError()
+                        .UseAssertionExceptionType<NUnit.Framework.AssertionException>()
+                        .UseNUnitAggregateAssertionStrategy().Build();
+
+            using (var dbContext = new AccountingEntities())
+            {
+                dbContext.Database.ExecuteSqlCommand("TRUNCATE TABLE [Budgets]");
+            }
+
+            _createBudgetPage = Go.To<CreateBudgetPage>();
+        }
+
+        [TearDown]
+        public void TearDown()
+        {
+            AtataContext.Current?.CleanUp();
+        }
+
+        [Test]
+        public void create_a_budget()
+        {
+            _createBudgetPage
+                .Create(new Budget() {YearMonth = "202003", Amount = 31m})
+                .Status.Should.ContainAll("created", "succeed");
+        }
+    }
+}
